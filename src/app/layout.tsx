9151a52b794346737d9ec4444e52019a70dcbf61
Mohamed Hassan <exorblack@gmail.com>
2024-04-01 17:14:36 +0200

    oks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import Link from "next/link";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         {children}
 
       </ThemeProvider>
+      <SpeedInsights />
       </body> 
     </html>
   );
