fb437959c00773a8fac59fdd87737106a960f809
Mohamed Hassan <exorblack@gmail.com>
2024-04-01 16:43:43 +0200

    onon

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -3,12 +3,12 @@ import Pokegrid from "@/components/pokegrid";
 import pokelist, { getpoke } from "@/lib/pokeapi";
 import pokemonpage from "../[pokemonname]/page"
 
-interface ok {
-  name:string
-  image:string
-}
+//interface ok {
+  //name:string
+  //image:string
+//}
 
-const pokemon = async ({name,image}: ok) => {
+const pokemon = async (image: string) => {
     const pokemonlist = await pokelist();
 
 
