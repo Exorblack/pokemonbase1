44827adb1f5aaddfae545911d31152eb93878b67
Mohamed Hassan <exorblack@gmail.com>
2024-04-02 11:40:32 +0200

    are

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,20 +1,21 @@
-import React from "react"; 
 import Pokegrid from "@/components/pokegrid";
 import pokelist from "@/lib/pokeapi";
 
-interface PokemonProps { 
-  name: string;
-  image: string;
+interface PokemonProps  {
+  name:string
+  image:string
 }
 
-const pokemon: React.FC<PokemonProps> = async ({ name }: PokemonProps) => {
-  const pokemonlist = await pokelist();
+const PokemonPage = async ({name}: PokemonProps) => {
+    const pokemonlist = await pokelist();
+
 
   return (
     <>
-      <Pokegrid pokelist={pokemonlist} image={name} /> 
+    <Pokegrid pokelist={pokemonlist} image ={name} />
     </>
-  );
-};
 
-export default pokemon;
+  )
+}
+
+export default PokemonPage
