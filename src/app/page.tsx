dcd5e4c3bbe4809752493d2d25a1479089e64178
Mohamed Hassan <exorblack@gmail.com>
2024-04-02 14:50:03 +0200

    edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
   return (
     <main>
       <div className="text-center gap-4 text-xl text-white pt-[100px]">we have More Than +250 pokemon </div>
-      <div className="text-2xl font-bold text-slate-200 text-center p-5"> find out yourself</div>
-      <Link href="/pokemon"><div className="text-center hover:underline text-gray-400">Click Here</div></Link>
+      <Link href="/pokemon"><div className="text-2xl font-bold text-gray-400 text-center p-5 hover:underline"> Find Out</div></Link>
     </main>
   );
 }
